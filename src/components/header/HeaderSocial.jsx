8b6ed7f7c3c9ab063d303377f36a676f93e0012d
Rofayda <roufybassem@gamil.com>
2023-06-28 01:23:15 +0300

    Fixing linkedIn redirection

--- src/components/header/HeaderSocial.jsx
+++ src/components/header/HeaderSocial.jsx
@@ -5,7 +5,7 @@ import { BsGithub } from 'react-icons/bs'
 export const HeaderSocial = () => {
   return (
     <div className='header__socials'>
-        <a href="linkedin.com/in/rofayda-bassem-03363b1a9" target='_blank'> <BsLinkedin size={40}/> </a>
+        <a href="https://linkedin.com/in/rofayda-bassem-03363b1a9" target='_blank'> <BsLinkedin size={40}/> </a>
         <a href="https://github.com/Rofaydaaa" target='_blank'> <BsGithub size={40}/> </a>
     </div>
   )
